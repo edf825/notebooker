4744b1fa8d634eb27a8a8a8a51d4254725f874a1
Jon Bannister <Jon.Bannister@man.com>
2019-01-15 15:17:02 +0000

    DS-67: Fixing latex specific packages

--- docker/install_man_notebooker.sh
+++ docker/install_man_notebooker.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -o xtrace -o errexit -o pipefail -o nounset
 
-yum -y install texlive-xetex texlive-xetex-def texlive-adjustbox texlive-upquote texlive-ulem && yum clean all
+yum -y install texlive-xetex \
+               texlive-ec \
+               texlive-collection-fontsrecommended \
+               texlive-collection-latexrecommended \
+               texlive-collection-xetex \
+               texlive-texconfig \
+               texlive-xetex-def \
+               texlive-adjustbox \
+               texlive-upquote \
+               texlive-ulem \
+           && yum clean all
 
 if ! req=$(ls /tmp/dist/*.egg 2>/dev/null); then
     req="man.notebooker==${VERSION}"
