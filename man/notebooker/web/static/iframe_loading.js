c567438219a9deb8531c766c627852ceaaddfd03
Jon Bannister <Jon.Bannister@man.com>
2019-05-14 12:20:06 +0100

    DS-528: Side-scrolling output fixed

--- man/notebooker/web/static/iframe_loading.js
+++ man/notebooker/web/static/iframe_loading.js
@@ -2,7 +2,6 @@ var iframeLoaded = function(iframe) {
     $(".iframeToLoad").show();
     setInterval(function(){
             iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-            iframe.style.width  = iframe.contentWindow.document.body.scrollWidth  + 'px';
     }, 1);
     $('.iframeLoadingDimmer').removeClass('active').addClass('disabled');
 
