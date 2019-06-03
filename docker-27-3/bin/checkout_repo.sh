bb4354872af4ab6c34a7a309732c4e2e0668310f
Jon Bannister <jon.bannister@man.com>
2019-06-03 13:07:42 +0100

    Creating two separate builds for 27-3 and 36-1

---
+++ docker-27-3/bin/checkout_repo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Checking out repo to $1"
+mkdir -p $1
+pushd $1
+git init
+git remote add origin "${NOTEBOOKER_TEMPLATE_GIT_URL}"
+git config core.sparseCheckout true
+echo "/${GIT_REPO_TEMPLATE_DIR}" >> .git/info/sparse-checkout
+git pull origin master
+# We should now have templates at ${PY_TEMPLATE_DIR}/${GIT_REPO_TEMPLATE_DIR}
+popd
+exit 0
---
+++ docker-27-3/install_man_notebooker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -o xtrace -o errexit -o pipefail -o nounset
+
+if ! req=$(ls /tmp/dist/*.egg 2>/dev/null); then
+    req="man.notebooker==${VERSION}"
+fi
+
+${MEDUSA_ENV}/bin/easy_install ${req}
