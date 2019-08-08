a82f20d9db8b85c3a88781b023fb7a207db32e6d
Dodds, Matthew (London) <Matthew.Dodds@man.com>
2019-08-08 14:05:19 +0100

    Clean up after ourselves

--- man/notebooker/utils/notebook_execution.py
+++ man/notebooker/utils/notebook_execution.py
@@ -26,37 +26,37 @@ def send_result_email(result, mailto):
     body = result.raw_html
     attachments = []
     tmp_dir = tempfile.mkdtemp(dir=os.path.expanduser('~'))
+    try:
+        if isinstance(result, NotebookResultComplete):
+            # Attach PDF output to the email. Has to be saved to disk temporarily for the mail API to work.
+            report_name = result.report_name.replace(os.sep, REPORT_NAME_SEPARATOR)
+            if isinstance(report_name, bytes):
+                report_name = report_name.decode('utf-8')
+            if result.pdf:
+                pdf_name = u'{}_{}.pdf'.format(report_name, result.job_start_time.strftime('%Y-%m-%dT%H%M%S'))
+                pdf_path = os.path.join(tmp_dir, pdf_name)
+                with open(pdf_path, 'w') as f:
+                    f.write(result.pdf)
+                attachments.append(pdf_path)
 
-    if isinstance(result, NotebookResultComplete):
-        # Attach PDF output to the email. Has to be saved to disk temporarily for the mail API to work.
-        report_name = result.report_name.replace(os.sep, REPORT_NAME_SEPARATOR)
-        if isinstance(report_name, bytes):
-            report_name = report_name.decode('utf-8')
-        if result.pdf:
-            pdf_name = u'{}_{}.pdf'.format(report_name, result.job_start_time.strftime('%Y-%m-%dT%H%M%S'))
-            pdf_path = os.path.join(tmp_dir, pdf_name)
-            with open(pdf_path, 'w') as f:
-                f.write(result.pdf)
-            attachments.append(pdf_path)
+            # Embed images into the email as attachments with "cid" links.
+            for resource_path, resource in result.raw_html_resources.get('outputs', {}).items():
+                resource_path_short = resource_path.rsplit(os.sep, 1)[1]
+                new_path = os.path.join(tmp_dir, resource_path_short)
+                with open(new_path, 'w') as f:
+                    f.write(resource)
 
-        # Embed images into the email as attachments with "cid" links.
-        for resource_path, resource in result.raw_html_resources.get('outputs', {}).items():
-            resource_path_short = resource_path.rsplit(os.sep, 1)[1]
-            new_path = os.path.join(tmp_dir, resource_path_short)
-            with open(new_path, 'w') as f:
-                f.write(resource)
+                body = re.sub(r'<img src="{}"'.format(resource_path),
+                              r'<img src="cid:{}"'.format(resource_path_short),
+                              body)
+                attachments.append(new_path)
 
-            body = re.sub(r'<img src="{}"'.format(resource_path),
-                          r'<img src="cid:{}"'.format(resource_path_short),
-                          body)
-            attachments.append(new_path)
+        msg = ['Please either activate HTML emails, or see the PDF attachment.', body]
 
-    msg = ['Please either activate HTML emails, or see the PDF attachment.', body]
-
-    logger.info(u'Sending email to %s with %d attachments', mailto, len(attachments))
-    mail(from_email, to_email, subject, msg, attachments=attachments)
-
-    shutil.rmtree(tmp_dir)
+        logger.info(u'Sending email to %s with %d attachments', mailto, len(attachments))
+        mail(from_email, to_email, subject, msg, attachments=attachments)
+    finally:
+        shutil.rmtree(tmp_dir)
 
 
 def mkdir_p(path):
