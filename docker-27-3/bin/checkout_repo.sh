479831eacee9672480824cc506f9cbdedee198c5
Santos, Tyler (Boston) <tyler.santos@man.com>
2019-08-27 12:01:01 -0400

    DATA-2273: ability to specify git ref

--- docker-27-3/bin/checkout_repo.sh
+++ docker-27-3/bin/checkout_repo.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-echo "Checking out repo to $1"
+checkout_ref=${CHECKOUT_GIT_HASH:-origin/master}
+echo "Checking out repo to $1 as of $checkout_ref"
 mkdir -p $1
 pushd $1
 git init
 git remote add origin "${NOTEBOOKER_TEMPLATE_GIT_URL}"
 git config core.sparseCheckout true
 echo "/${GIT_REPO_TEMPLATE_DIR}" >> .git/info/sparse-checkout
-git pull origin master
+git fetch
+git checkout $checkout_ref
 # We should now have templates at ${PY_TEMPLATE_DIR}/${GIT_REPO_TEMPLATE_DIR}
 popd
 exit 0
